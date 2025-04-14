209789e0fb2a939098b48bedb03a3a5a59c42269
Dex Douksy <31524651+DexDousky@users.noreply.github.com>
2025-04-13 22:12:26 -0300

    socorro?

---
+++ jogo.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame; //necessario para criar a janela.
+
+public class jogo {
+
+    public static void main(String[] args) {
+        JFrame janela = new JFrame();
+        janela.setSize(800, 800); 
+        janela.setVisible(true);
+    }
+}
