c77ade5c569f52724afcd887dab6a45cdcaf4d23
SrDakrFrame (João Victor Alves ) <jvalvessouza2005@gmail.com>
2025-05-18 03:09:49 -0300

    adicionando as mudanças

--- src/main/java/main/Arvore.java
+++ src/main/java/main/Arvore.java
@@ -22,14 +22,28 @@ class SpritesAparte {
         arvore[0] = ImageIO.read(getClass().getClassLoader().getResourceAsStream("assets/arvoreNormal.png"));
         arvore[1] = ImageIO.read(getClass().getClassLoader().getResourceAsStream("assets/arvoreMeioQueimada.png"));
         arvore[2] = ImageIO.read(getClass().getClassLoader().getResourceAsStream("assets/arvoreQueimando.png"));
-        arvore[1] = ImageIO.read(getClass().getClassLoader().getResourceAsStream("assets/arvoreCarbonizada.png"));
+        arvore[3] = ImageIO.read(getClass().getClassLoader().getResourceAsStream("assets/arvoreCarbonizada.png"));
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    
+    public void desenhar(java.awt.Graphics g,int x,int y){
+        if (arvore[0] != null){
+            g.drawImage(arvore[0], x, y, null);
+        }
+        if (arvore[1] !=null) {
+            g.drawImage(arvore[1], x, y, null);
+        }
+        if (arvore[2] != null){
+            g.drawImage(arvore[2],x,y, null);
+        }
+        if (arvore[3] != null){
+            g.drawImage(arvore[3], x, y, null);
         }
     }
     public void atualizar(int novaVar){
 
         this.variacao = Math.abs(novaVar) % arvore.length;
     }
-
 }
